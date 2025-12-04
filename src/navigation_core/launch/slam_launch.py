a7f116694c6aa1dd3ba5fe99c48ac6d934b37cca
KlasLundgren <klasf.lundgren@gmail.com>
2025-12-05 00:50:22 +0100

    Moved som stuff and created launch file

--- src/navigation_core/launch/slam_launch.py
+++ src/navigation_core/launch/slam_launch.py
@@ -9,6 +9,7 @@ from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 
 
+
 def generate_launch_description():
     # Get the package directories
     navigation_core_dir = get_package_share_directory('navigation_core')
